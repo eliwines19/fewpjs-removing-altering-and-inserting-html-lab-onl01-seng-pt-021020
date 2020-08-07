42b03479451738e60e729c4199c3b699f15d4a2b
Elijah wines <elijahwines84@yahoo.com>
2020-08-07 13:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// Write your code here!
+// Write your code here
 
-let ul = document.createElement('ul')
-
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-main.remove();
-
-let newHeader = document.createElement('h1');
-document.body.appendChild(newHeader);
-
-newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = ''
+document.getElementById('main').remove();
